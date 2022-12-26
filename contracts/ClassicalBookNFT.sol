cc08bf04d0183112014f61353f6ec254868ae93e
daxuanxiaoyou <yangsai_2006@163.com>
2022-12-26 10:10:35 +0800

    Update ClassicalBookNFT.sol

--- contracts/ClassicalBookNFT.sol
+++ contracts/ClassicalBookNFT.sol
@@ -3,37 +3,76 @@ pragma solidity ^0.8.8;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/security/PullPayment.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import '@openzeppelin/contracts/security/PullPayment.sol';
+import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 
-contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
+contract ClassicalBookNFT is ERC721, Ownable, ReentrancyGuard, PullPayment {
     using Counters for Counters.Counter;
     Counters.Counter private currentTokenId;
 
+    using EnumerableSet for EnumerableSet.AddressSet;
+
     // Constants
     uint256 public maxSupply = 1;
+    // mint price
+    uint256 public mintPrice = 0.2 ether;
 
     /// @dev Base token URI used as a prefix by tokenURI().
-    string public baseTokenURI;
+    string private baseTokenURI;
+    // switch mint
+    bool public isMintEnabled = true;
+    /// store tokenid --> bookId
+    mapping(uint256 => string) public tokenToBook;
+    // used for switch mint
+    EnumerableSet.AddressSet private whitelist;
 
-    event mintEvent(uint256, string, address);
+    modifier onlyWhitelist() {
+        require(whitelist.contains(_msgSender()), "not in whitelist!");
+        _;
+    }
+
+    event Mint(address indexed user, uint256 indexed tokenId, string bookId);
+    event SetMintPrice(uint256 price);
 
     // name and symbol
-    constructor() ERC721("Classical Book NFT Collectioin", "ClassicalBook") {}
+    constructor(string memory baseUri) ERC721("Classical Book NFT Collectioin", "ClassicalBook") {
+        //drop 0
+        currentTokenId.increment();
+        baseTokenURI = baseUri;
+    }
+
+    function toggleIsMintEnabled() external onlyWhitelist {
+        isMintEnabled = !isMintEnabled;
+    }
+    
+    /// the lowest price is 1e15 Wei
+    function setMintPrice(uint256 newPriceUnit) external onlyOwner {
+        mintPrice = newPriceUnit * 1e15;
+        emit SetMintPrice(newPriceUnit);
+    }
 
     function mintTo(address recipient, string memory bookId)
         public
         payable
+        nonReentrant
         returns (uint256)
     {
+        require(isMintEnabled, 'Minting not enabled');
+        require(msg.value == mintPrice, "Please set the right value");
+  
         uint256 tokenId = currentTokenId.current();
-        require(tokenId < maxSupply, "Max supply reached");
-
+        require(tokenId <= maxSupply, "Max supply reached");
+        //transfer eth to the contract
+        _asyncTransfer(address(this), msg.value);
+        // mint nft
+        _safeMint(recipient, tokenId);
+        // tokenId ++
         currentTokenId.increment();
-        uint256 newTokenId = currentTokenId.current();
-        emit mintEvent(newTokenId, bookId, recipient);
-        _safeMint(recipient, newTokenId);
-        return newTokenId;
+        tokenToBook[tokenId] = bookId;
+        emit Mint(recipient, tokenId, bookId);
+        return tokenId;
     }
 
     /// @dev Returns an URI for a given token ID
@@ -48,6 +87,8 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
 
     /// Sets max supply, one book one nft
     function setMaxSupply(uint256 _maxSupply) public onlyOwner {
+        uint256 tokenId = currentTokenId.current();
+        require(_maxSupply > tokenId, "Must greate than current token id");
         maxSupply = _maxSupply;
     }
 
@@ -59,10 +100,6 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
         return tokenId;
     }
 
-    function getMaxSupply() public view returns (uint256) {
-        return maxSupply;
-    }
-
     /// @dev Overridden in order to make it an onlyOwner function
     function withdrawPayments(address payable payee)
         public
@@ -72,4 +109,21 @@ contract ClassicalBookNFT is ERC721, PullPayment, Ownable {
     {
         super.withdrawPayments(payee);
     }
+
+    function addToWhitelist(address[] calldata _addrArr) external onlyOwner {
+        for (uint256 i = 0; i < _addrArr.length; i++) {
+            require(whitelist.add(_addrArr[i]), "Add whitelist failed!");
+        }
+    }
+
+    function removeFromWhitelist(address[] calldata _addrArr) external onlyOwner {
+        for (uint256 i = 0; i < _addrArr.length; i++) {
+            require(whitelist.remove(_addrArr[i]), "Remove whitelist failed!");
+        }
+    }
+
+    function getWhitelist() external view returns (address[] memory) {
+        return whitelist.values();
+    }
+
 }
